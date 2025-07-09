615d6c833415389085088d24dd85e0e9b4c68081
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-09 18:16:13 +0530

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -1,1 +1,9 @@
+from sklearn.metrics import classification_report, ConfusionMatrixDisplay
+import matplotlib.pyplot as plt
+
+def evaluate_model(model, X_test, y_test):
+    y_pred = model.predict(X_test)
+    print("Classification Report:\n", classification_report(y_test, y_pred))
+    ConfusionMatrixDisplay.from_estimator(model, X_test, y_test)
+    plt.show()
 
