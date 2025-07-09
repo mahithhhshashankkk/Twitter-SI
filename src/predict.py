0d5882681b6701fca12a945a8a8b38dbf89488ae
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-09 18:16:26 +0530

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -1,1 +1,10 @@
+import re
+
+def preprocess_text(texts):
+    return [re.sub(r'\W+', ' ', t.lower()) for t in texts]
+
+def predict_texts(model, vectorizer, texts):
+    processed = preprocess_text(texts)
+    X = vectorizer.transform(processed)
+    return model.predict(X)
 
