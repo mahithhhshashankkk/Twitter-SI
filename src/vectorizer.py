dcacccae671f5551cc93ab67eeba217e521a6bfe
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-09 18:15:41 +0530

    Update vectorizer.py

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -1,1 +1,8 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from config import TFIDF_PARAMS
+
+def extract_features(texts):
+    vectorizer = TfidfVectorizer(**TFIDF_PARAMS)
+    X = vectorizer.fit_transform(texts)
+    return X, vectorizer
 
