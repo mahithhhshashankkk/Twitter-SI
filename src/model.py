d4fd4c2b092e5d75d5beb6e555552ae5accb84c9
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-09 18:16:00 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -1,1 +1,12 @@
+from sklearn.linear_model import LogisticRegression
+from config import RANDOM_SEED, MAX_ITER
+
+def train_model(X_train, y_train):
+    model = LogisticRegression(
+        max_iter=MAX_ITER, 
+        class_weight='balanced', 
+        random_state=RANDOM_SEED
+    )
+    model.fit(X_train, y_train)
+    return model
 
