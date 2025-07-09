2116a8f141c90657d436829fc7b0da9a49df09b2
mahithhhshashankkk <162897517+mahithhhshashankkk@users.noreply.github.com>
2025-07-09 18:15:22 +0530

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,11 @@
+import pandas as pd
+import re
+
+def load_and_clean_data(path):
+    df = pd.read_csv(path)
+    df.rename(columns={'Tweet': 'text', 'Suicide': 'label'}, inplace=True)
+    df.dropna(subset=['text', 'label'], inplace=True)
+    df['text'] = df['text'].str.lower().apply(lambda x: re.sub(r'\W+', ' ', x))
+    return df
+
 
